6bfe0220092cfeb4199f0af67c8c14032873cac2
Israel Franca <icfranc@hotmail.com>
2025-03-20 18:29:42 -0300

    Atualzação Script

--- iac1.sh
+++ iac1.sh
@@ -21,23 +21,24 @@ groupadd GRP_SEC
 
 echo "Criando os usuários do grupo GRP_ADM."
 sleep 1
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
 
 echo " Criando os usuários do grupo GRP_VEN"
 sleep 1
 
-useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd debora -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
 
 echo "Criando usuários do grupo GRP_SEC"
 sleep 1
 
-useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd josefina -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
 
 echo "Especificando as permissões dos diretórios."
 
